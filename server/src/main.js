45210f1f97af2928f3d4182f9dea096820ce03a7
Kevin Pitti Castro <kepc08@gmail.com>
2023-12-26 11:45:11 -0600

    add promisify to spawn process function

--- server/src/main.js
+++ server/src/main.js
@@ -4,21 +4,50 @@ import path from 'path';
 
 const currentDirectory = cwd()
 
-const pythonCmd = path.join(currentDirectory, 'python', '.venv', 'scripts', 'python')
-const pythonFilePath = path.join(currentDirectory, 'python', 'src', 'main.py')
-
-const pythonChildProcess = spawn(`${pythonCmd}`, [pythonFilePath])
-
-pythonChildProcess.stdout.on('data', async (data) => {
-  const audioInfo = JSON.parse(data.toString())
-  console.log('Audio info:', audioInfo);
-})
-
-pythonChildProcess.on('exit', (...args) => {
-  console.log(args);
-})
+const PYTHON_CMD = path.join(currentDirectory, 'python', '.venv', 'scripts', 'python')
+const PYTHON_FILE_PATH = path.join(currentDirectory, 'python', 'src', 'main.py')
 
 
-pythonChildProcess.on('error', (...args) => {
-  console.log(args);
-})
+function analyzeAudio({ audioPath }) {
+
+  const spawnProcess = spawn(`${PYTHON_CMD}`, [PYTHON_FILE_PATH, audioPath])
+  let response = ''
+  
+  const promise = new Promise((resolve, reject) => {
+    spawnProcess.stdout.on('data', (data) => {
+      response += data.toString()
+    })
+
+    spawnProcess.stdout.on('end', () => {
+      try {
+        console.log(response)
+        const { status, data } = JSON.parse(response)
+        if(status === 'fail') reject()
+
+        resolve(data)
+      } catch (e) {
+        console.error('error', e)
+        reject()
+      }
+    })
+    
+    spawnProcess.stderr.on('data', (data) => {
+      console.error(data.toString());
+    })
+     
+    spawnProcess.on('error', (err) => {
+      console.error(err);
+      reject()
+    })
+  })
+
+  return promise
+}
+
+
+try {
+  const audioInfo = await analyzeAudio({audioPath: "C:/Users/kepc0/Downloads/y2mate.com - Barak  Acepta Video Oficial.mp3"})
+  console.log(audioInfo);
+} catch (error) {
+  console.log(error)
+}

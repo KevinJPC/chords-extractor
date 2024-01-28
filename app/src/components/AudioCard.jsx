fdf995682d8af7051d1eb4cd2d84f15a05e6d2c3
Kevin Pitti Castro <kepc08@gmail.com>
2024-01-28 13:25:39 -0600

    add audio card breakpoint

--- app/src/components/AudioCard.jsx
+++ app/src/components/AudioCard.jsx
@@ -1,16 +1,25 @@
+import { useMemo } from 'react'
 import './AudioCard.css'
 import { chordParserFactory, chordRendererFactory } from 'chord-symbol/lib/chord-symbol.js'
 
-const parseChord = chordParserFactory()
-const renderChord = chordRendererFactory()
+// const CHORDS_PARSER_OPTIONS = {
+//   useShortNamings: true,
+//   simplify: 'none', // none | max | core,
+//   transposeValue: 0,
+//   accidental: 'origianal', // original | flat | sharp
+//   notationSystem: 'english'
+// }
+
+// const parseChord = useMemo(() => chordParserFactory(), [])
+// const renderChord = useMemo(() => chordRendererFactory(CHORDS_PARSER_OPTIONS), [])
+
+// console.log(chordsPerBeats)
+// const chordsSymbol = chordsPerBeats?.map((chord) => {
+//   const chordsParsed = parseChord(chord)
+//   return renderChord(chordsParsed)
+// })
 
 export const AudioCard = ({ _id, youtubeId, title, thumbnail, edits, props, chordsPerBeats }) => {
-  // console.log(chordsPerBeats)
-
-  const chord = parseChord('B/G')
-  console.log(chord)
-  console.log(renderChord(chord))
-
   const isAnalyzed = _id !== undefined
   const statusText = isAnalyzed ? 'analyzed' : 'not analyzed'
   const statusClass = isAnalyzed ? 'audio-card__footer--analyzed' : 'audio-card__footer--not-analyzed'
@@ -21,24 +30,33 @@ export const AudioCard = ({ _id, youtubeId, title, thumbnail, edits, props, chor
       </figure>
       <div className='audio-card__info'>
         <h1 className='audio-card__title'>{title}</h1>
-        {
+        <div className='audio-card__details'>
+          {
           isAnalyzed
             ? (
-              <ul className='audio-card__details-list'>
-                <li className='audio-card__details-item'>
-                  {edits} edits
-                </li>
-              </ul>
+              <>
+                <ul className='audio-card__list'>
+                  <li className='audio-card__list-item'>
+                    {33} visits
+                  </li>
+                  <li className='audio-card__list-item'>
+                    {edits} edits
+                  </li>
+                </ul>
+              </>
               )
+
             : (
               <button type='submit' className='audio-card__analyze-button'>
                 Analyze now
               </button>
               )
           }
-        <footer className={`audio-card__footer ${statusClass}`}>
-          {statusText}
-        </footer>
+          <footer className={`audio-card__footer ${statusClass}`}>
+            {statusText}
+          </footer>
+        </div>
+
       </div>
     </article>
   )

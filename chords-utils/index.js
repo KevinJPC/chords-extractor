badc45aa9d533e0d4de4ec28345fee365206f237
Kevin Pitti Castro <kepc08@gmail.com>
2024-03-04 17:40:51 -0600

    refactor: move functions to its own workspace

---
+++ chords-utils/index.js
@@ -0,0 +1,36 @@
+import chordSymbol from 'chord-symbol/lib/chord-symbol.js'
+
+const { chordParserFactory, chordRendererFactory } = chordSymbol
+
+export const formatChordSymbols = (chordSymbols, { simplify = false, transpose = 0 }) => {
+  const parseChord = chordParserFactory({ notationSystems: ['english'] })
+  const renderChord = chordRendererFactory({
+    useShortNamings: true,
+    simplify: simplify === true ? 'max' : 'none',
+    transposeValue: transpose
+  })
+  const formattedChordSymbols = chordSymbols.map((chordSymbol) => {
+    const chordObj = parseChord(chordSymbol)
+    const formattedChordSymbol = renderChord(chordObj)
+    return formattedChordSymbol
+  })
+
+  return formattedChordSymbols
+}
+
+export const validateAndFormatChordSymbols = (chordSymbols) => {
+  const parseChord = chordParserFactory({ notationSystems: ['english'] })
+  const renderChord = chordRendererFactory({ useShortNamings: true })
+  const formattedChordSymbols = []
+  const invalidChordSymbols = new Set()
+  for (let chordSymbol of chordSymbols) {
+    if (chordSymbol !== null) {
+      const chordObj = parseChord(chordSymbol)
+      if (chordObj.error !== undefined) invalidChordSymbols.add(chordSymbol)
+      chordSymbol = renderChord(chordObj)
+    }
+    formattedChordSymbols.push(chordSymbol)
+  }
+
+  return { invalidChordSymbols: Array.from(invalidChordSymbols), formattedChordSymbols }
+}

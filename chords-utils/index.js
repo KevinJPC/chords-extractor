6d6a2363b90c9a0f5196470f0ec40446d6a013e1
Kevin Pitti Castro <kepc08@gmail.com>
2024-03-05 17:35:36 -0600

    feat: add notes info

--- chords-utils/index.js
+++ chords-utils/index.js
@@ -34,3 +34,23 @@ export const validateAndFormatChordSymbols = (chordSymbols) => {
 
   return { invalidChordSymbols: Array.from(invalidChordSymbols), formattedChordSymbols }
 }
+
+export const notes = {
+  AFlat: 'Ab',
+  A: 'A',
+  ASharp: 'A#',
+  BFlat: 'Bb',
+  B: 'B',
+  C: 'C',
+  CSharp: 'C#',
+  DFlat: 'Db',
+  D: 'D',
+  DSharp: 'D#',
+  EFlat: 'Eb',
+  E: 'E',
+  F: 'F',
+  FSharp: 'F#',
+  GFlat: 'Gb',
+  G: 'G',
+  GSharp: 'G#'
+}

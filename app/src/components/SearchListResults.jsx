abe5027b7777405dc792ca999b4f3502fa36f6bd
Kevin Pitti Castro <kepc08@gmail.com>
2024-03-07 17:23:38 -0600

    feat: add job states to parent component and navigation features

--- app/src/components/SearchListResults.jsx
+++ app/src/components/SearchListResults.jsx
@@ -1,11 +1,11 @@
-import { createContext, useContext, useEffect, useState } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { createAudioAnalysisJob, getAudioAnalysisJob } from '../services/audioAnalyses'
 import { AudioCard } from './AudioCard'
 import './SearchListResults.css'
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { Spinner } from './Spinner'
-import { useLocation } from 'wouter'
+import { Link, useLocation } from 'wouter'
 import { useMutation, useQuery } from '@tanstack/react-query'
 
 const ANALYSIS_STATUS = {
@@ -18,88 +18,104 @@ const ANALYSIS_STATUS = {
 const SearchListResultsContext = createContext()
 
 export const SearchListResults = ({ children, ...props }) => {
-  const [youtubeIdAnalyzing, setYoutubeIdAnalyzing] = useState(null)
+  const [selectedResultId, setSelectedResultId] = useState(null)
+  const [, navigate] = useLocation()
 
-  const { isSuccess: mutationIsSuccess, isPending: mutationIsPending, data: mutationData, error, mutate: runAudioAnalysisJobMutation } = useMutation({
+  const { isSuccess: mutationIsSuccess, isPending: mutationIsPending, data: mutationData, error: mutationError, mutate: runAudioAnalysisJobMutation } = useMutation({
     mutationFn: createAudioAnalysisJob
   })
-  const { data: queryData } = useQuery({
+  const { data: queryData, error: queryError } = useQuery({
     queryKey: ['job'],
     queryFn: () => getAudioAnalysisJob({ jobId: mutationData.id }),
     enabled: mutationIsSuccess && mutationData?.id !== null && ['waiting', 'processing'].includes(mutationData?.status),
     refetchInterval: (query) => {
-      // console.log(query)
       if (['waiting', 'processing'].includes(query.state.data?.status)) return 1000
       return false
-    }
+    },
+    refetchOnWindowFocus: false
   })
 
-  const job = mutationData !== undefined || queryData !== undefined ? { ...mutationData, ...queryData } : undefined
+  const { jobResult, jobIsError, jobIsInQueue, jobIsProcessing, jobIsSuccess } = useMemo(() => {
+    const job = mutationData !== undefined || queryData !== undefined ? { ...mutationData, ...queryData } : undefined
+    const jobIsError = mutationError !== null || queryError !== null || job?.status === 'error'
+    const jobIsInQueue = job?.status === 'waiting'
+    const jobIsProcessing = job?.status === 'processing'
+    const jobIsSuccess = job?.status === 'completed'
+    return { jobResult: job?.result, jobIsError, jobIsInQueue, jobIsProcessing, jobIsSuccess }
+  }, [mutationData, queryData, mutationError, queryError])
+
+  useEffect(() => {
+    if (!jobIsSuccess) return
+    const timeoutId = setTimeout(() => {
+      navigate(`chords/${jobResult._id}`)
+    }, 1000)
+
+    return () => clearTimeout(timeoutId)
+  }, [jobIsSuccess])
 
   const analyze = ({ youtubeId }) => {
-    if (youtubeIdAnalyzing !== null) return
-    setYoutubeIdAnalyzing(youtubeId)
-    // setAnalysisStatus(ANALYSIS_STATUS.starting)
+    console.log(selectedResultId && !jobIsError)
+    if (selectedResultId && !jobIsError) return
+    setSelectedResultId(youtubeId)
     runAudioAnalysisJobMutation({ youtubeId })
   }
-  ///
-  const [, navigate] = useLocation()
 
-  const handleOnNavigate = ({ _id }) => {
-    if (youtubeIdAnalyzing !== null || _id === undefined) return
-    console.log('navigating')
-    // navigate(`chords/${youtubeId}`)
-  }
   return (
-    <SearchListResultsContext.Provider value={{ youtubeIdAnalyzing, analyze, mutationIsPending, mutationIsSuccess, job }}>
+    <SearchListResultsContext.Provider value={{ selectedResultId, analyze, mutationIsPending, jobIsError, jobIsInQueue, jobIsProcessing, jobIsSuccess }}>
       {children}
     </SearchListResultsContext.Provider>
   )
 }
 
 SearchListResults.Item = ({ youtubeId, thumbnails, title, duration, audioAnalysis, isAnalyzed }) => {
-  const { youtubeIdAnalyzing, analyze, mutationIsPending, mutationIsSuccess, job } = useContext(SearchListResultsContext)
-  const isAnalyzing = youtubeIdAnalyzing === youtubeId
+  const { selectedResultId, analyze, mutationIsPending, jobIsError, jobIsInQueue, jobIsProcessing, jobIsSuccess } = useContext(SearchListResultsContext)
 
-  const isInQueue = isAnalyzing && (job?.status === 'waiting')
-  const isProcessing = isAnalyzing && (job?.status === 'processing')
-  const hasSucceded = isAnalyzing && (job?.status === 'completed')
+  const isSelectedResult = selectedResultId === youtubeId
+
   const showDetailList = isAnalyzed
-  const showButton = !isAnalyzed && (!isAnalyzing || job === undefined)
-  const buttonContent = isAnalyzing && mutationIsPending ? <Spinner size={14} /> : 'Analyze now'
-  const showStatus = !isAnalyzing || job === undefined
+  const showButton = !isAnalyzed && (!isSelectedResult || mutationIsPending || jobIsError)
+  const buttonContent = isSelectedResult && mutationIsPending ? <Spinner size={14} /> : 'Analyze now'
+  const showStatus = !isSelectedResult || mutationIsPending || jobIsError
+
+  const bpm = Math.round(audioAnalysis?.bpm)
 
   return (
-    <AudioCard>
-      <AudioCard.Thumbnail>
-        <AudioCard.ThumbnailImg src={thumbnails[0].url} />
-      </AudioCard.Thumbnail>
-      <AudioCard.Content>
-        <AudioCard.Title>{title}</AudioCard.Title>
-        <AudioCard.Body>
+    <Link to={isAnalyzed ? `chords/${audioAnalysis._id}` : ''} replace={isAnalyzed === false}>
+      <a>
+        <AudioCard>
+          <AudioCard.Thumbnail>
+            <AudioCard.ThumbnailImg src={thumbnails[0].url} />
+          </AudioCard.Thumbnail>
+          <AudioCard.Content>
+            <AudioCard.Title>{title}</AudioCard.Title>
+            <AudioCard.Body>
 
-          {isInQueue && 'waiting in queue'}
-          {isProcessing && 'processing'}
-          {hasSucceded && 'redirecting'}
+              {showDetailList && (
+                <AudioCard.DetailsList>
+                  <AudioCard.DetailsItem>{12} edits</AudioCard.DetailsItem>
+                  <AudioCard.DetailsItem>{12} views</AudioCard.DetailsItem>
+                  {audioAnalysis && <AudioCard.DetailsItem> {bpm} bpm</AudioCard.DetailsItem>}
+                </AudioCard.DetailsList>
+              )}
 
-          {showDetailList && (
-            <AudioCard.DetailsList>
-              <AudioCard.DetailsItem>{12} edits</AudioCard.DetailsItem>
-              <AudioCard.DetailsItem>{12} views</AudioCard.DetailsItem>
-            </AudioCard.DetailsList>
-          )}
+              {showButton && (
+                <AudioCard.Button onClick={() => analyze({ youtubeId })}>
+                  {buttonContent}
+                </AudioCard.Button>
+              )}
 
-          {showButton && (
-            <AudioCard.Button onClick={() => analyze({ youtubeId })}>
-              {buttonContent}
-            </AudioCard.Button>
-          )}
+              {isSelectedResult && jobIsError && 'An error happend, try again.'}
+              {isSelectedResult && jobIsInQueue && 'waiting in queue'}
+              {isSelectedResult && jobIsProcessing && 'processing'}
+              {isSelectedResult && jobIsSuccess && 'redirecting'}
 
-          {showStatus && <AudioCard.Status isAnalyzed={isAnalyzed} />}
+              {showStatus && <AudioCard.Status isAnalyzed={isAnalyzed} />}
 
-        </AudioCard.Body>
-      </AudioCard.Content>
-    </AudioCard>
+            </AudioCard.Body>
+          </AudioCard.Content>
+        </AudioCard>
+      </a>
+    </Link>
   )
 }
 
